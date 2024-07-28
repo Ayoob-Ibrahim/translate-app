b6f9033907baaf93bfe22d30b63adcec1a1cda82
eitayoob <ayoobeitworks.com>
2024-07-28 15:10:14 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'translate-application';
+  supportedLanguage = ['en', 'ar', 'ta', 'fr']
+  constructor(private trasnServ: TranslateService) {
+    this.trasnServ.addLangs(this.supportedLanguage);
+    this.trasnServ.setDefaultLang('ta');
+
+    // this one takes the user browser language and run the appliaction in a required specific way
+    const browserLangu: any = this.trasnServ.getBrowserLang();
+    this.trasnServ.use(browserLangu)
+  }
+
+
+  selectln(data: any) {
+    this.trasnServ.use(data.target.value)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core'
+import { TranslateHttpLoader } from '@ngx-translate/http-loader'
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (http: HttpClient) => {
+          return new TranslateHttpLoader(http, './assets/language/')
+        },
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
